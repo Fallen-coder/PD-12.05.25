4be694d192002a7eadaf9d9a7be2e1638f32e673
Your Name <you@example.com>
2025-05-12 16:15:10 +0300

    added delete with destroy method

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -2,3 +2,8 @@
 <h3>{{ $book->author }}</h3>
 <p>{{ $book->released_at }}</p>
 <a href="/books">All books</a>
+<form action="/books/{{ $book->id }}/destroy" method="post">
+    @csrf 
+    @method('delete')
+    <input type="submit" value="Delete">
+</form>
