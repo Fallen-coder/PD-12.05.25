e85eeb03693f0f618e2093061aa554f9344fcd08
Your Name <you@example.com>
2025-05-12 15:42:38 +0300

    fixed return view ,fixed so it take it to view and took 2 unneeded ['books' => ]

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,7 +9,7 @@ class BookController extends Controller
 {
     public function index() {
         $books = Book::all();
-        return view('books.index');
+        return view('books.index',['books' => $books]);
     }
 
     public function create() {
@@ -23,17 +23,17 @@ class BookController extends Controller
             'released_at' => $request['released_at'],
         ]);
 
-        return redirect('/books' . $book->id);
+        return redirect('/books');
     }
 
     public function show($id) {
         $book = Book::find($id);
-        return view('books.show', ['singleBook' => $book]);
+        return view('books.show', ['book' => $book]);
     }
 
     public function edit($id) {
         $book = Book::find($id);
-        return $book;
+        return view('books.edit', ['book' => $book]);
     }
 
     public function update(Request $request, $id) {
@@ -44,6 +44,6 @@ class BookController extends Controller
             'released_at' => $request['released_at'],
         ]);
 
-        return redirect('/books' . $book->id);
+        return redirect('/books');
     }
 }
